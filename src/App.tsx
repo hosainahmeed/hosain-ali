ed820a45b8a6b088a5d2f5f038571f4dd42008a8
hosainahmeed <skyfal430@gmail.com>
2026-03-07 10:30:38 +0600

    update noise effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import FooterSection from './components/FooterSection';
 import HeaderSection from "./components/HeaderSection";
 import HeroSection from "./components/HeroSection";
-import ProjectSection from "./components/ProjectSection";
 import SkillsSection from "./components/SkillsSection";
-// import TimeLine from "./components/TimeLine";
 
 
 export default function App() {
@@ -13,10 +11,22 @@ export default function App() {
         <HeaderSection />
         <HeroSection />
         <SkillsSection />
-        <ProjectSection />
+        {/* <ProjectSection /> */}
         {/* <TimeLine /> */}
         <FooterSection />
+      </div>
+      <div className="fixed top-0 pointer-events-none opacity-15 left-0 w-full h-screen">
+        <svg viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'>
+          <filter id='noiseFilter'>
+            <feTurbulence
+              type='fractalNoise'
+              baseFrequency='7.18'
+              numOctaves='4'
+              stitchTiles='stitch' />
+          </filter>
 
+          <rect width='100%' height='100%' filter='url(#noiseFilter)' />
+        </svg>
       </div>
     </>
   );
